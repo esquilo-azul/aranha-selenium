8c25f3dc25a0b5862cf689878ce0cae20732c08d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-04-15 19:44:23 -0300

    Aranha::Selenium::Session.

--- lib/aranha/selenium/session.rb
+++ lib/aranha/selenium/session.rb
@@ -17,6 +17,13 @@ module Aranha
         )
       end
 
+      # @return [Selenium::WebDriver::Element, nil]
+      def find_element(*args, &block)
+        return args.first if args.count >= 1 && args.first.is_a?(::Selenium::WebDriver::Element)
+
+        __getobj__.find_element(*args, &block)
+      end
+
       def find_or_not_element(find_element_args)
         r = find_elements(find_element_args)
         r.any? ? r.first : nil
