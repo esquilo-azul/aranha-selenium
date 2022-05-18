699d9e404806781d6c0819916976f249f8e547b9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-05-18 12:57:21 -0300

    Aranha::Selenium::Session.select_option*.

--- lib/aranha/selenium/session.rb
+++ lib/aranha/selenium/session.rb
@@ -29,6 +29,20 @@ module Aranha
         r.any? ? r.first : nil
       end
 
+      def select_option(field, value, *find_element_args)
+        select = find_element(*find_element_args)
+        option = ::Selenium::WebDriver::Support::Select.new(select)
+        option.select_by(field, value)
+      end
+
+      def select_option_by_text(text, *find_element_args)
+        select_option(:text, text, *find_element_args)
+      end
+
+      def select_option_by_value(value, *find_element_args)
+        select_option(:value, value, *find_element_args)
+      end
+
       def wait_for_click(find_element_args)
         wait.until do
           element = find_element(find_element_args)
