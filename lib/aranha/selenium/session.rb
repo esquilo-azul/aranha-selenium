69bf5da99e23d61b2b57ec9f1fabecf661159f11
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-10-16 16:07:34 -0300

    Aranha::Selenium::Session::Wait: extract from Aranha::Selenium::Session.

--- lib/aranha/selenium/session.rb
+++ lib/aranha/selenium/session.rb
@@ -7,45 +7,16 @@ module Aranha
   module Selenium
     class Session < ::SimpleDelegator
       require_sub __FILE__, include_modules: true
-      attr_reader :downloads, :wait
+      enable_simple_cache
+      attr_reader :downloads
 
       def initialize(options = {})
         @downloads = Downloads.new
-        @wait = ::Selenium::WebDriver::Wait.new(timeout: 15)
         super(
           ::Aranha::Selenium::DriverFactory.create_driver(
             options.merge(download_dir: @downloads.dir)
           )
         )
-      end
-
-      def wait_for_click(find_element_args)
-        wait.until do
-          element = find_element(find_element_args)
-          element ? element_click(element) : nil
-        end
-      end
-
-      def wait_for_element(find_element_args)
-        wait.until { find_element(find_element_args) }
-      end
-
-      def wait_for_download
-        initial_downloads = downloads.current
-        yield
-        new_downloads = []
-        wait.until do
-          new_downloads = downloads.current - initial_downloads
-          new_downloads.any?
-        end
-        new_downloads.first
-      end
-
-      def wait_for_url_change(&block)
-        previous_url = current_url
-        block&.call
-
-        wait.until { current_url != previous_url }
       end
 
       def current_source
---
+++ lib/aranha/selenium/session/wait.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Aranha
+  module Selenium
+    class Session < ::SimpleDelegator
+      module Wait
+        def wait_for_click(find_element_args)
+          wait.until do
+            element = find_element(find_element_args)
+            element ? element_click(element) : nil
+          end
+        end
+
+        def wait_for_element(find_element_args)
+          wait.until { find_element(find_element_args) }
+        end
+
+        def wait_for_download
+          initial_downloads = downloads.current
+          yield
+          new_downloads = []
+          wait.until do
+            new_downloads = downloads.current - initial_downloads
+            new_downloads.any?
+          end
+          new_downloads.first
+        end
+
+        def wait_for_url_change(&block)
+          previous_url = current_url
+          block&.call
+
+          wait.until { current_url != previous_url }
+        end
+
+        private
+
+        def wait_uncached
+          ::Selenium::WebDriver::Wait.new(timeout: 15)
+        end
+      end
+    end
+  end
+end
