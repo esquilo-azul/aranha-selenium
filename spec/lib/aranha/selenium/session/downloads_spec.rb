4cfdf9b687f590a6aab757d851b7af1b41ee5666
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-12-06 13:03:47 -0300

    RSpec.describe(Aranha::Selenium::Session::Downloads).

---
+++ spec/lib/aranha/selenium/session/downloads_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'aranha/selenium/session/downloads'
+
+RSpec.describe Aranha::Selenium::Session::Downloads do
+  let(:instance) { described_class.new }
+  let(:file_path) { File.join(instance.dir, 'stub_file') }
+
+  it do
+    expect(instance.current).not_to include(file_path)
+  end
+
+  context 'when a closed file is added' do
+    before do
+      File.write(file_path, 'Any content')
+    end
+
+    it do
+      expect(instance.current).to include(file_path)
+    end
+
+    context 'when a the files is open' do
+      around do |example|
+        File.open(file_path, 'w') do
+          example.run
+        end
+      end
+
+      it do
+        expect(instance.current).to include(file_path)
+      end
+    end
+  end
+end
