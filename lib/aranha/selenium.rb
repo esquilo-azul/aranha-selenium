62736b7cdf93f8c8474e98390a420d4c942471c7
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-03-11 20:26:16 -0300

    Aranha::Selenium: refact with .required_sub.

--- lib/aranha/selenium.rb
+++ lib/aranha/selenium.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/core_ext'
+
 module Aranha
   module Selenium
-    require 'aranha/selenium/driver_factory'
-    require 'aranha/selenium/session'
+    require_sub __FILE__
   end
 end
