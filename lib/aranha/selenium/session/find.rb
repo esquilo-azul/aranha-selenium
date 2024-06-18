30b38a6caaa4848e04aef68b232d76246654e60b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-06-17 22:32:51 -0300

    Aranha::Selenium::Session::Find.find_or_not_element: fix.

--- lib/aranha/selenium/session/find.rb
+++ lib/aranha/selenium/session/find.rb
@@ -17,7 +17,7 @@ module Aranha
         # @param find_element_args [Array] Argujments for +Selenium::WebDriver::Find.find_elements+.
         # @deprecated Use +{#element}.find+ instead.
         # @return [Selenium::WebDriver::Element, nil]
-        def find_or_not_element(find_element_args)
+        def find_or_not_element(*find_element_args)
           element(*find_element_args).find
         end
       end
